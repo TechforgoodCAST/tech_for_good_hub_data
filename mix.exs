10f273b98fc0b13d60e258151b1c41c580c11c40
David Kane <d.r.kane@gmail.com>
2017-04-04 12:51:07 +0100

    Add slim as a template manager

--- mix.exs
+++ mix.exs
@@ -37,6 +37,7 @@ defmodule TechForGoodHub.Mixfile do
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
+     {:phoenix_slime, "~> 0.8.0"},
      {:cowboy, "~> 1.0"}]
   end
 
