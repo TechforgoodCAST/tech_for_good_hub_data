ba6ca3d0e9e100f06d8ddddffa22a11acf8de9ac
Suraj Vadgama <contact@suninthesky.co.uk>
2017-04-07 12:35:57 +0100

    Add static version of report resolves #13

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -5,4 +5,9 @@ defmodule TechForGoodHub.PageControllerTest do
     conn = get conn, "/"
     assert html_response(conn, 200) =~ "Tech For Good"
   end
+
+  test "GET /analysis", %{conn: conn} do
+    conn = get conn, "/analysis"
+    assert html_response(conn, 200) =~ "Comic Relief Tech for Good"
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,7 @@ defmodule TechForGoodHub.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+    get "/analysis", PageController, :analysis
 
     resources "/organisations", OrganisationController
 
