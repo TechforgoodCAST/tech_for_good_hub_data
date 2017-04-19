9e0f7320d10863f2a564b405b85a5ccba823e73f
David Kane <d.r.kane@gmail.com>
2017-04-19 15:43:02 +0100

    Add organisation to test model

--- test/controllers/proposal_controller_test.exs
+++ test/controllers/proposal_controller_test.exs
@@ -2,6 +2,7 @@ defmodule TechForGoodHub.ProposalControllerTest do
   use TechForGoodHub.ConnCase
 
   alias TechForGoodHub.Proposal
+  alias TechForGoodHub.Organisation
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, proposal_path(conn, :index)
@@ -9,7 +10,10 @@ defmodule TechForGoodHub.ProposalControllerTest do
   end
 
   test "shows chosen resource", %{conn: conn} do
-    proposal = Repo.insert! %Proposal{}
+    organisation = Repo.insert! %Organisation{}
+    proposal = Repo.insert! %Proposal{organisation_id: organisation.id}
+    proposal = Repo.get!(Proposal, proposal.id)
+               |> Repo.preload([:organisation, :tags])
     conn = get conn, proposal_path(conn, :show, proposal)
     assert html_response(conn, 200) =~ "Proposal from"
   end
