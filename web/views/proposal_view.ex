40020923f0446de1bce26a6435a27407a9cd59fc
David Kane <d.r.kane@gmail.com>
2017-04-19 11:51:37 +0100

    Add status and region filters (with styling)

--- web/views/proposal_view.ex
+++ web/views/proposal_view.ex
@@ -18,6 +18,29 @@
     |> Phoenix.HTML.raw
   end
 
+  def render_regions() do
+    # @TODO shouldn't be hardcoded here, duplicated with models/proposal.ex:69
+    regions = %{
+      "region-london" => "London",
+      "region-north-west" => "North West",
+      "region-north-east" => "North East",
+      "region-south-west" => "South West",
+      "region-south-east" => "South East",
+      "region-yorkshire-and-the-humber" => "Yorkshire and the Humber",
+      "region-east-midlands" => "East Midlands",
+      "region-eastern" => "Eastern",
+      "region-west-midlands" => "West Midlands",
+      "region-scotland" => "Scotland",
+      "region-northern-ireland" => "Northern Ireland",
+      "region-wales" => "Wales"
+    }
+    regions
+    |> Enum.map(fn {region_slug, region} ->
+      "<option value='#{region_slug}'>#{region}</option>"
+    end)
+    |> Phoenix.HTML.raw
+  end
+
   def categories do
     [
       %{ name: "Approach", category: "approach-type" },
